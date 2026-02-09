2b5fd666769995e09f2e83208d774761a50fdb3d
Lopez-JhonLay <jhonlaylopez000@gmail.com>
2026-02-09 18:05:05 +0800

    test  Please enter the commit message for your changes. Lines starting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import ChatBot from './components/chat/ChatBot';
 function App() {
   return (
     <div className="w-3xl mx-auto p-4 h-screen">
+      <div>My ChatBot</div>
       <ChatBot />
     </div>
   );
